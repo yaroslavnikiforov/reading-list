67cf78684abe2b8e0553d9367c3623783d44d874
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-08-08 23:02:36 +0300

    Navbar and Booklist components

---
+++ src/components/Booklist/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Booklist extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Cashflow quadrant</li>
+          <li>The power of the moment now</li>
+          <li>Rich Dad, poor Dad</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
