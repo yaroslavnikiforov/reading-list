628087043fca357d15a50680d2c762134d11766e
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-08-10 22:59:47 +0300

    Adding a Context & Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import ThemeContextProvider from "./contexts/ThemeContext";
 import Navbar from "./components/Navbar";
 import Booklist from "./components/Booklist";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div className="app">
+      <ThemeContextProvider>
+        <Navbar />
 
-      <Booklist />
+        <Booklist />
+      </ThemeContextProvider>
     </div>
   );
 }
