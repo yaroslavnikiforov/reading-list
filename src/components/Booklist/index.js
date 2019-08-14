bed048067248754c57bc60f541ccdfc2cd4e60d0
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-08-14 23:04:46 +0300

    Hooks with Context

--- src/components/Booklist/index.js
+++ src/components/Booklist/index.js
@@ -1,30 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 
 export default function Booklist() {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+
   return (
-    <ThemeContext.Consumer>
-      {context => {
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Cashflow quadrant</li>
 
-        return (
-          <div
-            className="book-list"
-            style={{ background: theme.bg, color: theme.syntax }}
-          >
-            <ul>
-              <li style={{ background: theme.ui }}>Cashflow quadrant</li>
+        <li style={{ background: theme.ui }}>The power of the moment now</li>
 
-              <li style={{ background: theme.ui }}>
-                The power of the moment now
-              </li>
-
-              <li style={{ background: theme.ui }}>Rich Dad, poor Dad</li>
-            </ul>
-          </div>
-        );
-      }}
-    </ThemeContext.Consumer>
+        <li style={{ background: theme.ui }}>Rich Dad, poor Dad</li>
+      </ul>
+    </div>
   );
 }
