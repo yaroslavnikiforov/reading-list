3a3a3d3d38647382d28f0fb170dca15afe9378f2
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-08-18 21:20:02 +0300

    Updates

---
+++ precache-manifest.c82d5491454d1660d920b5c5847e6510.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5727de105c68f5fdc7773a63f885305",
+    "url": "/reading-list/index.html"
+  },
+  {
+    "revision": "a1bb2e17c45231920a1f",
+    "url": "/reading-list/static/css/main.5f35883c.chunk.css"
+  },
+  {
+    "revision": "a2174061bdcae79ed4c0",
+    "url": "/reading-list/static/js/2.ce74c2de.chunk.js"
+  },
+  {
+    "revision": "a1bb2e17c45231920a1f",
+    "url": "/reading-list/static/js/main.55cd6274.chunk.js"
+  },
+  {
+    "revision": "1517d8eadda4992a81ea",
+    "url": "/reading-list/static/js/runtime~main.1df08123.js"
+  }
+]);
